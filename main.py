a9d93ee6b04fde47e6161db99423ac4621c71361
Asadbek Saydullayev <134209600+AsadbekSaydu11ayev@users.noreply.github.com>
2025-01-06 22:35:42 +0500

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,10 @@ col1, col2, col3 = st.columns(3)
 with col1:
     st.image(PILImage.create("person1_bacteria_2.jpeg"), width=100)
 with col2:
-    st.image(PILImage.create("person2_bacteria_4.jpeg"), width=100)
+    st.image(PILImage.create("person2_bacteria_4.jpeg"), width=95)
 with col3:
-    st.image(PILImage.create("person1_bacteria_12.jpeg"), width=100)
+    st.image(PILImage.create("person3_bacteria_12.jpeg"), width=100)
+    
 file = st.file_uploader(label="Rasm yuklash", type=["Jpg", "Png", "Svg","Gif"])
 
 
