875dc295c23c204a06907cfcb074390383412d49
Asadbek Saydullayev <134209600+AsadbekSaydu11ayev@users.noreply.github.com>
2025-05-23 22:01:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -20,24 +20,24 @@ with col3:
     
 file = st.file_uploader(label="Rasm yuklash", type=["Jpg", "Png", "Svg","Gif"])
 
-model = load_learner("pnevmaniya_model.pkl")
+
 if file:
     try:
         img = PILImage.create(file)
 
-        # model = load_learner("pnevmaniya_model.pkl")
+        model = load_learner("pnevmaniya_model.pkl")
         pred, pred_id, probs = model.predict(img)
 
 
-        # st.success(f"Bashorat: {pred}")
-        # st.info(f"Ehtimollik: {probs[pred_id]*100:.1f}%")    
-        # st.image(img, width=400)
+        st.success(f"Bashorat: {pred}")
+        st.info(f"Ehtimollik: {probs[pred_id]*100:.1f}%")    
+        st.image(img, width=400)
 
 
-        # fig = plt.figure(figsize=(10,4))
-        # sns.barplot(x=probs*100, y=model.dls.vocab)
-        # plt.xlabel("Ehtimollik")
-        # st.pyplot(fig)
+        fig = plt.figure(figsize=(10,4))
+        sns.barplot(x=probs*100, y=model.dls.vocab)
+        plt.xlabel("Ehtimollik")
+        st.pyplot(fig)
 
     except:
         st.text("Siz rasm yuklamadingiz. Iltimos Rasm yuklang:)")
